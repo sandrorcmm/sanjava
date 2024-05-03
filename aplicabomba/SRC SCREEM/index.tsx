8328a29405b75e0bc7fb6609cd889a6ebd1244af
sandrorcmm <159553071+sandrorcmm@users.noreply.github.com>
2024-05-03 11:54:04 -0300

    dsafavs
    fgbsd

--- aplicabomba/SRC SCREEM/index.tsx
+++ aplicabomba/SRC SCREEM/index.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 
-export default function App() {
+export default function SingnIn() {
     return(
         <View>
-            <Text></Text>
+            <Text>Teste do aplicabomba</Text>
         </View>
     )
 }

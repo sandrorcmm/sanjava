d1a9de27410b78ac68d0d87e460e17fe3eef8495
sandrorcmm <159553071+sandrorcmm@users.noreply.github.com>
2024-05-08 11:58:29 -0300

    dfassadfv
    fvfs

--- aplicabomba/App.tsx
+++ aplicabomba/App.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
 import { View, Text } from 'react-native';
+import { SingIn } from './src/screen/singin';
 
 export default function App() {
     return(
-        <View>
-            <Text></Text>
-        </View>
+        <SingIn />
     )
 }

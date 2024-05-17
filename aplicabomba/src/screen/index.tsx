f49a314db259493906521c6befb73b523676a8ec
sandrorcmm <159553071+sandrorcmm@users.noreply.github.com>
2024-05-17 12:00:09 -0300

    yhfdnhxf
    gfn

--- aplicabomba/src/screen/index.tsx
+++ aplicabomba/src/screen/index.tsx
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { View, Text,TextInput } from 'react-native';
 import { styles } from './styles';
 
+
 export function SingnIn() {
+    const [text, setText] = useState ("chama");
+
+
     return(
         <View style={styles.container}>
-            <Text>APLICABOMBA</Text>
-            <TextInput style={styles.input} />
+            <Text>A vida é um eterno oscilar entre a ânsia de ter e o tédio de possuir</Text>
+
+
+            <TextInput style={styles.input} onChangeText={setText} />
+
+            <Text>
+                Você digitou: {text}
+            </Text>
         </View>
     )
 }
